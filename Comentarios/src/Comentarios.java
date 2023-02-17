4212d3bdd5f5441afd3d90d415a09bb7bf062bf2
VacaErick <erickyaelvacagorostieta@gmail.com>
2023-02-16 23:54:14 -0600

    new file:   Comentarios/src/Comentarios.java

---
+++ Comentarios/src/Comentarios.java
@@ -0,0 +1,9 @@
+public class Comentarios {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hola mundo"); // esta es una comentario de una linea
+        /*
+         * esta es una comentario de multiples lineas
+         * en la linea 3 imprimimos hola mundo en pantalla 
+         */
+    }
+}
