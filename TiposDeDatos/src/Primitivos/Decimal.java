80e1a8c623ab189c339381e9b2df74e9eaa4c1be
VacaErick <erickyaelvacagorostieta@gmail.com>
2023-02-19 13:49:01 -0600

    new file:   TiposDeDatos/bin/Primitivos/Decimal.class 	new file:   TiposDeDatos/bin/Primitivos/Decimal.png 	new file:   TiposDeDatos/src/Primitivos/Decimal.java 	new file:   TiposDeDatos/src/Primitivos/Decimal.png

---
+++ TiposDeDatos/src/Primitivos/Decimal.java
@@ -0,0 +1,9 @@
+package Primitivos;
+
+public class Decimal {
+    public static void main(String[] args) {
+        double decimalDouble = 3.1416;
+        float decimalFloat = 3.1416f;
+        System.out.println("double:" + decimalDouble + "\nfloat:" + decimalFloat);
+    }
+}
