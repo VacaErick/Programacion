0691f81fbfe68abbc6d5b2f26550dfb1180015a9
VacaErick <erickyaelvacagorostieta@gmail.com>
2023-02-19 16:58:59 -0600

    new file:   TiposDeDatos/bin/Primitivos/Booleano.class 	new file:   TiposDeDatos/bin/Primitivos/Booleano.png 	new file:   TiposDeDatos/src/Primitivos/Booleano.java 	new file:   TiposDeDatos/src/Primitivos/Booleano.png

---
+++ TiposDeDatos/src/Primitivos/Booleano.java
@@ -0,0 +1,9 @@
+package Primitivos;
+
+public class Booleano {
+    public static void main(String[] args) {
+        boolean booleanoTrue = true;
+        boolean booleanoFalse = false;
+        System.out.println("true:" + booleanoTrue + "false:" + booleanoFalse);
+    }
+}
