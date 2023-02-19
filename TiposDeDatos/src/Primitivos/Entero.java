0e2c44362f6a8762b2058b1ab1119bf821b02466
VacaErick <erickyaelvacagorostieta@gmail.com>
2023-02-19 13:00:00 -0600

    new file:   TiposDeDatos/src/Primitivos/Entero.png 	new file:   TiposDeDatos/.vscode/settings.json 	new file:   TiposDeDatos/bin/Primitivos/Entero.class 	new file:   TiposDeDatos/bin/Primitivos/Entero.png 	new file:   TiposDeDatos/src/Primitivos/Entero.java

---
+++ TiposDeDatos/src/Primitivos/Entero.java
@@ -0,0 +1,12 @@
+package Primitivos;
+
+public class Entero {
+    public static void main(String[] args) {
+        byte enteroByte = 127;
+        short enteroShort = 32767;
+        int enteroInt = 2147483647;
+        long enteroLong = 92233720;
+        System.out.println(
+                "byte:" + enteroByte + "\nshort:" + enteroShort + "\nint:" + enteroInt + "\nlong:" + enteroLong);
+    }
+}
