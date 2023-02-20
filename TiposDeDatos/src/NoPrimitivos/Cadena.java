c24a0eb6c6fb14e3170d6928b15a3eea7070dae8
VacaErick <erickyaelvacagorostieta@gmail.com>
2023-02-19 18:52:50 -0600

    new file:   TiposDeDatos/bin/NoPrimitivos/Cadena.class 	new file:   TiposDeDatos/bin/NoPrimitivos/Cadena.png 	new file:   TiposDeDatos/src/NoPrimitivos/Cadena.java 	new file:   TiposDeDatos/src/NoPrimitivos/Cadena.png

---
+++ TiposDeDatos/src/NoPrimitivos/Cadena.java
@@ -0,0 +1,9 @@
+package NoPrimitivos;
+
+public class Cadena {
+    public static void main(String[] args) {
+        String cadenaNula = null;
+        String cadena = "chain";
+        System.out.println("the chain is: " + cadena + "\nthe value is: " + cadenaNula);
+    }
+}
