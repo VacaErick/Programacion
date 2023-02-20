fe2648026efa0bff4be492b613bde733424670ae
VacaErick <erickyaelvacagorostieta@gmail.com>
2023-02-19 20:08:14 -0600

    new file:   Constantes/.vscode/settings.json 	new file:   Constantes/Constantes.png 	new file:   Constantes/bin/Constantes.class 	new file:   Constantes/src/Constantes.java

---
+++ Constantes/src/Constantes.java
@@ -0,0 +1,6 @@
+public class Constantes {
+    public static void main(String[] args) throws Exception {
+        final double constantePi = 3.1416;
+        System.out.println(constantePi);
+    }
+}
